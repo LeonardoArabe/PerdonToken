852bce3cb986e3255deade282d8ee2a24381f0e6
Leonardo Arabe Castedo <a2020122243@estudiantes.upsa.edu.bo>
2021-04-10 11:12:47 -0400

    Se agregó el proyecto MVC Web Token

---
+++ PersonToken/WebToken/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebToken.Startup))]
+namespace WebToken
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
